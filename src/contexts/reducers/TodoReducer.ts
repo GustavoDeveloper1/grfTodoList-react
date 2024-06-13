7bf7aff50e72bf3de686cbbd89d0383feee1b6e6
Gustavo.Rocha <devinvestidor02@gmail.com>
2024-06-13 17:32:33 -0300

    criando contexts

---
+++ src/contexts/reducers/TodoReducer.ts
@@ -0,0 +1,18 @@
+import { TodoActions, TodoState } from "../../@types/TodoReducer";
+
+
+export const initialState: TodoState = [];
+
+
+export const TodoReducer = (state: TodoState, action: TodoActions): TodoState => {
+
+    switch(action.type) {
+        case 'ADD':
+
+break;
+    
+        default:
+return state
+    }
+
+}
